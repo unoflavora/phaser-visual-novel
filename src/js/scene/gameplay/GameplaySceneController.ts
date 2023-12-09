5bbcf6bca7d03da4a5770cf2a8ab1b90dcf056c2
prothegee <prothegee@outlook.com>
2023-12-09 17:33:57 +0700

    call a to set default active scene index

--- src/js/scene/gameplay/GameplaySceneController.ts
+++ src/js/scene/gameplay/GameplaySceneController.ts
@@ -6,6 +6,7 @@ import { MinigameTypes } from "Definitions/Minigame";
 import MainSceneController from "Scenes/MainSceneController";
 import { SceneState } from "Definitions/GameProgress";
 import ConsoleHelper from "Modules/helpers/ConsoleHelper";
+import StorageHelper from "Modules/helpers/StorageHelper";
 
 export default class GameplaySceneController extends Phaser.Scene {
 	
@@ -88,6 +89,8 @@ export default class GameplaySceneController extends Phaser.Scene {
 		ConsoleHelper.Log("FINISHED NOVEL");
 
 		ConsoleHelper.Log(MainSceneController.instance.gameData.progress.emotionalUnderstanding.scores);
+
+        StorageHelper.FinishedActiveSceneIndex();
 
 		await MainSceneController.instance.FinishMinigames();
 	}
